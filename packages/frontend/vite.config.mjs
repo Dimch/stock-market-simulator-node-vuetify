bebe5c649329b87b161d63ca90864afdb81a4a25
Dima Mic <dimcha@gmail.com>
2026-04-17 18:10:22 +0300

    running docker with app volumes attached to host for easy development

--- packages/frontend/vite.config.mjs
+++ packages/frontend/vite.config.mjs
@@ -12,9 +12,9 @@ import Vuetify, {transformAssetUrls} from 'vite-plugin-vuetify';
 import {defineConfig} from 'vite';
 import {fileURLToPath, URL} from 'node:url';
 
-if (!process.env.BACKEND_PORT) {
-  console.warn('BACKEND_PORT environment variable is not set. Defaulting to 3001.');
-  process.env.BACKEND_PORT = 3001;
+if (!process.env.BACKEND_BASE_URL) {
+  console.warn('BACKEND_BASE_URL environment variable is not set. Defaulting to 3001.');
+  process.env.BACKEND_BASE_URL = 'http://localhost:3001';
 }
 if (!process.env.PORT) {
   console.warn('PORT environment variable is not set. Defaulting to 3000.');
@@ -22,7 +22,7 @@ if (!process.env.PORT) {
 }
 
 // Proxy configuration helper functions
-const backendUrl = () => `http://localhost:${process.env.BACKEND_PORT}`;
+const backendUrl = () => process.env.BACKEND_BASE_URL;
 const backendConf = () => ({
   target: backendUrl(),
   changeOrigin: true,
