5ccaca0dddb76bba683fe98adda4a700a3beca35
Dima Mic <dimcha@gmail.com>
2026-04-21 00:53:47 +0300

    first commit
    add Postgres container to docker setup
    create seeding scripts

---
+++ packages/backend/database/pg/seed/01_market_schema.sql
@@ -0,0 +1,6 @@
+-- Create schema for market data
+create schema market;
+comment on schema market is 'SStock market specific data, e.g. list of stocks and prices';
+alter schema market owner to postgres;
+
+
---
+++ packages/backend/database/pg/seed/02_staff_schema.sql
@@ -0,0 +1,48 @@
+-- Create schema for administration staff data
+create schema staff;
+comment on schema staff is 'Website''s administration console related data, e.g. users and rate limits';
+alter schema staff owner to postgres;
+
+create table staff.users
+(
+    id        serial
+        constraint users_pk
+            primary key,
+    name      varchar(255)              not null,
+    email     varchar(255)              not null,
+    password  text                      not null,
+    crated_at timestamptz default now() not null
+);
+comment on table staff.users is 'Administration staff';
+alter table staff.users owner to postgres;
+create unique index users_email_uindex
+    on staff.users (email);
+
+create table staff.rate_limits
+(
+    id         serial
+        constraint rate_limits_pk
+            primary key,
+    key        varchar(100)              not null,
+    salt       text        default '' not null,
+    buckets    jsonb       default '{}'  not null,
+    updated_at timestamptz default now() not null
+);
+alter table staff.rate_limits owner to postgres;
+create unique index rate_limits_key_salt_uindex on staff.rate_limits (key, salt);
+
+create table staff.rate_limit_configs
+(
+    key         varchar(100)              not null
+        constraint rate_limit_configs_pk
+            primary key,
+    name        varchar(100)              not null,
+    description text                      not null,
+    max         decimal                   not null,
+    size        integer                   not null,
+    width       integer                   not null,
+    unit        varchar(50)               not null,
+    updated_at  timestamptz default now() not null
+);
+alter table staff.rate_limit_configs owner to postgres;
+
